1de069271c76dea1926c56db901b3a6a32a87b08
John Chaffee <johnchaffee@mac.com>
2021-09-13 22:04:40 -0700

    Handle undefined event.Body

--- functions/event-to-webhook.js
+++ functions/event-to-webhook.js
@@ -1,46 +1,53 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 exports.handler = async function (context, event, callback) {
   // const twiml = new Twilio.twiml.MessagingResponse();
 
+  const body = '';
+
+  if (event.Body === undefined) {
+    // do nothing, body is already set to ''
+  } else {
+    body = event.Body;
+  }
+
   try {
-    
     console.log(context);
     console.log(event);
 
     // Translate Twilio event properties to Zipwhip event properties
     const zwEvent = {
-      "cc": null,
-      "visible": true,
-      "read": false,
-      "address": "ptn:/" + event.From,
-      "bcc": null,
-      "contactId": null,
-      "finalDestination": event.To,
-      "scheduledDate": null,
-      "body": event.Body,
-      "deviceId": null,
-      "dateDeleted": null,
-      "messageTransport": 5,
-      "dateDelivered": null,
-      "hasAttachment": (event.NumMedia > 0) ? true : false,
-      "dateCreated": new Date().toISOString().slice(0,-5)+"Z",
-      "deleted": false,
-      "messageType": "MO",
-      "bodySize": 14,
-      "fingerprint": null,
-      "id": event.SmsSid,
-      "dateRead": null,
-      "finalSource": event.From,
-      "statusCode": 4,
-      "bodySize": event.Body.length ||= 0,
+      cc: null,
+      visible: true,
+      read: false,
+      address: "ptn:/" + event.From,
+      bcc: null,
+      contactId: null,
+      finalDestination: event.To,
+      scheduledDate: null,
+      body: body,
+      deviceId: null,
+      dateDeleted: null,
+      messageTransport: 5,
+      dateDelivered: null,
+      hasAttachment: event.NumMedia > 0 ? true : false,
+      dateCreated: new Date().toISOString().slice(0, -5) + "Z",
+      deleted: false,
+      messageType: "MO",
+      bodySize: 14,
+      fingerprint: null,
+      id: event.SmsSid,
+      dateRead: null,
+      finalSource: event.From,
+      statusCode: 4,
+      bodySize: body.length,
     };
 
     // Send Zipwhip formatted webhook
     const res = await fetch(context.WEBHOOK_URL, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       // body: JSON.stringify(event),
       body: JSON.stringify(zwEvent),
