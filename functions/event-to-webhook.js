d288f8d8471be13db729c941b383a83d880a5a5c
John Chaffee <johnchaffee@mac.com>
2021-09-13 17:22:46 -0700

    Add zwEvent

--- functions/event-to-webhook.js
+++ functions/event-to-webhook.js
@@ -1,25 +1,55 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function (context, event, callback) {
-  const twiml = new Twilio.twiml.MessagingResponse();
+  // const twiml = new Twilio.twiml.MessagingResponse();
 
   try {
-    // IFTTT only pulls the fields value1, value2, and value3 from webhook JSON;
-    event.value1 = event.MessageSid;
-    event.value2 = event.From;
-    event.value3 = event.Body;
+    
+    console.log(context);
+    console.log(event);
 
+    // Translate Twilio event properties to Zipwhip event properties
+    const zwEvent = {
+      "cc": null,
+      "visible": true,
+      "read": false,
+      "address": "ptn:/" + event.From,
+      "bcc": null,
+      "contactId": null,
+      "finalDestination": event.To,
+      "scheduledDate": null,
+      "body": event.Body,
+      "deviceId": null,
+      "dateDeleted": null,
+      "messageTransport": 5,
+      "dateDelivered": null,
+      "hasAttachment": (event.NumMedia > 0) ? true : false,
+      "dateCreated": new Date().toISOString().slice(0,-5)+"Z",
+      "deleted": false,
+      "messageType": "MO",
+      "bodySize": 14,
+      "fingerprint": null,
+      "id": event.SmsSid,
+      "dateRead": null,
+      "finalSource": event.From,
+      "statusCode": 4,
+      "bodySize": event.Body.length,
+    };
+
+    // Send Zipwhip formatted webhook
     const res = await fetch(context.WEBHOOK_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(event),
+      // body: JSON.stringify(event),
+      body: JSON.stringify(zwEvent),
     });
 
     if (res.ok) {
-      twiml.message('The SMS was successfully forwarded to your webhook.');
-      return callback(null, twiml);
+      // twiml.message('The SMS was successfully forwarded to your webhook.');
+      // return callback(null, twiml);
+      return callback(null);
     }
 
     return callback(res.statusText);
